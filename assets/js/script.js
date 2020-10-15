55fda98664a7e2a125a63050ff650a43fe527913
Gocus10 <37048598+Gocus10@users.noreply.github.com>
2020-10-14 21:42:23 -0500

    basic layout

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+var questionEl = document.querySelector("#question");
+var startGameEl = document.querySelector("#start-button");
+var correctEl = document.querySelector("#correct");
+var wrongEl = document.querySelector("wrong");
+var questionId = 0;
+
+var questionHandler = function(event) {
+    
+}
+var createTaskEl = function(taskDataObj) {
+
+}
